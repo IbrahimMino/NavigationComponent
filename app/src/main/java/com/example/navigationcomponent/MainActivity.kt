04c9e4ae881a318bc8a441b2dfef36c78aa74920
Android developer <im1999yil@gmail.com>
2021-03-11 18:32:52 +0500

    Navigation Componenet with Bottom Navigation

--- app/src/main/java/com/example/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponent/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.navigationcomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationMenu
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNaviMenu = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController =  findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.firstFragment,R.id.secondFragment,R.id.thridFragment))
+          setupActionBarWithNavController(navController,appBarConfiguration)
+        bottomNaviMenu.setupWithNavController(navController)
+
+
     }
 }
